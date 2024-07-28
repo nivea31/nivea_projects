9ee8f743819c5c842895f2b03046b617c8b9f50f
nivea31 <75798446+nivea31@users.noreply.github.com>
2024-07-28 23:00:47 +0530

    Add files via upload

---
+++ practiceday2/Encapsulationdemo.java
@@ -0,0 +1,17 @@
+package practiceday2;
+
+public class Encapsulationdemo {
+
+	public static void main(String[] args) {
+		
+		Oopproject obj = new Oopproject();// object creation
+		obj.setAge(23);
+		obj.setName("Nivea");
+		obj.setSerialNum(1);
+		System.out.println(obj.getAge());
+		System.out.println(obj.getName());
+		System.out.println(obj.getSerialNum());
+		
+	}
+
+}
