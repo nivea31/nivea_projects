4c6c66c5984fefa34739d896fee9988549d351ba
nivea31 <75798446+nivea31@users.noreply.github.com>
2024-07-29 17:14:16 +0530

    Add files via upload

---
+++ example/InheritanceDemo.java
@@ -0,0 +1,24 @@
+package com.tnsif.example;
+
+public class InheritanceDemo {
+	
+}
+class Parent
+{
+	void property()
+	{
+		System.out.println("Cash,Gold,Plots");
+	}
+	void marry()
+	{
+		System.out.println("Aman");
+	}
+}
+class Child extends Parent
+{
+	void marry()//method overriding
+	{
+		System.out.println("Sameer");
+	}
+	
+}
